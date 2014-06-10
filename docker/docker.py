a5ef2e9dc627caea4aa6bb75372c879b2eb56dfc
Konstantin Molchanov <moigagoo@live.com>
2014-06-11 00:03:18 +0400

    Docker: 0.0.1: Extension created.

---
+++ docker/docker.py
@@ -0,0 +1,51 @@
+﻿__title__ = 'sloth-ci.ext.docker'
+__version__ = '0.0.1'
+__author__ = 'Konstantin Molchanov'
+__license__ = 'MIT'
+
+
+import docker
+
+
+def extend(cls):
+    class Sloth(cls):
+        def __init__(self, config):
+            super().__init__(config)
+
+            self.docker_client = docker.Client()
+            self.image = self.name
+
+        def execute(self, action):
+            """Execute an action inside a container, then commit the changes to the image and remove the container.
+
+            :param action: action to be executed
+
+            :returns: True if successful, Exception otherwise
+            """
+
+            self.processing_logger.info('Executing action: %s', action)
+
+            try:
+                try:
+                    container_id = self.docker_client.create_container(self.name, command=action)['Id']
+
+                except docker.APIError as e:
+                        if e.response.status_code == 404:
+                            self.docker_client.build(config['docker']['path_to_dockerfile'], tag=self.image)
+                            self.processing_logger.info('Image built: %s' % self.image)
+
+                            container_id = self.docker_client.create_container(self.name, command=action)['Id']
+                        else:
+                            raise
+
+                self.docker_client.start(container_id)
+                self.docker_client.commit(container_id, self.image)
+                self.docker_client.remove_container(container_id)
+
+                self.processing_logger.info('Action executed: %s', action)
+                return True
+
+            except Exception:
+                raise
+
+    return Sloth
---
+++ docker/setup.py
@@ -0,0 +1,28 @@
+﻿from setuptools import setup
+
+import docker as ext
+
+
+package = 'sloth_ci.ext'
+
+setup(
+    name=ext.__title__,
+    version=ext.__version__,
+    author=ext.__author__,
+    description='Docker app extension for Sloth CI',
+    long_description='Docker Sloth CI app extension that replaces the default executor with a Docker-based one.',
+    author_email='moigagoo@live.com',
+    url='https://bitbucket.org/moigagoo/sloth-ci-extensions',
+    py_modules=['%s.docker' % package],
+    package_dir={package: '.'},
+    install_requires = [
+        'sloth_ci>=0.6.2'
+    ],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3']
+    )
