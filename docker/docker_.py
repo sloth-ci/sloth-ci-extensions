b22ce20131e04f774538efe1602c652871a1417e
Konstantin Molchanov <moigagoo@live.com>
2014-06-11 23:09:18 +0400

    Docker: 0.0.9: Module file renamed; new params added - image, base_url

--- docker/docker_.py
+++ docker/docker_.py
@@ -1,5 +1,5 @@
 ﻿__title__ = 'sloth-ci.ext.docker'
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 __author__ = 'Konstantin Molchanov'
 __license__ = 'MIT'
 
@@ -12,8 +12,8 @@ def extend(cls):
         def __init__(self, config):
             super().__init__(config)
 
-            self.docker_client = Client()
-            self.image = self.name
+            self.docker_client = Client(self.config['docker'].get('base_url'))
+            self.image = self.config['docker'].get('image') or self.name
 
         def execute(self, action):
             """Execute an action inside a container, then commit the changes to the image and remove the container.
@@ -50,7 +50,11 @@ def extend(cls):
                         raise
 
                 self.docker_client.start(container_id)
-                self.docker_client.commit(container_id, self.image)
+                self.docker_client.commit(
+                    container_id,
+                    self.image,
+                    message=action
+                )
                 self.docker_client.remove_container(container_id)
 
                 self.processing_logger.info('Action executed: %s', action)
--- docker/setup.py
+++ docker/setup.py
@@ -1,6 +1,6 @@
 ﻿from setuptools import setup
 
-import docker_exec as ext
+import docker_ as ext
 
 
 package = 'sloth_ci.ext'
