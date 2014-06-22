fd4f4ddc70a198ad4142be2b8050ef39dbc02ceb
Konstantin Molchanov <moigagoo@live.com>
2014-06-22 14:58:29 +0400

    Logs: 0.0.4: Error in import fixed.

--- logs/logs.py
+++ logs/logs.py
@@ -1,7 +1,7 @@
 __title__ = 'sloth-ci.ext.logs'
 __description__ = 'Logs for Sloth CI apps'
 __long_desciption__ = 'Sloth CI extension that adds a logger (can be rotating) to Sloth CI apps.'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __author__ = 'Konstantin Molchanov'
 __author_email__ = 'moigagoo@live.com'
 __license__ = 'MIT'
@@ -27,7 +27,7 @@ def extend(cls):
             )
             
             if log_config.get('rotating'):
-                file_handler = logging.RotatingFileHandler(
+                file_handler = logging.handlers.RotatingFileHandler(
                     abspath(join(self.config['log_dir'], self.name + '.log')),
                     'a+',
                     maxBytes=log_config.get('max_bytes') or 0,
