a81b4dc8905374685cc8bb14ffb44b1bdb21d3ca
Konstantin Molchanov <moigagoo@live.com>
2014-06-22 13:31:39 +0400

    Logs: 0.0.1: Logs extension created.

---
+++ logs/logs.py
@@ -0,0 +1,41 @@
+__title__ = 'sloth-ci.ext.html-logs'
+__description__ = 'Logs for Sloth CI apps'
+__long_desciption__ = 'Sloth CI extension that adds a logger (can be rotating) to Sloth CI apps.'
+__version__ = '0.0.1'
+__author__ = 'Konstantin Molchanov'
+__author_email = 'moigagoo@live.com'
+__license__ = 'MIT'
+
+
+"""Sloth CI extension that adds a logger (can be rotating and timed rotating) to Sloth CI apps."""
+
+
+from os.path import abspath, join
+
+import logging
+
+
+def extend(cls):
+    class Sloth(cls):
+        def __init__(self, config):
+            super().__init__(config)
+            
+            formatter = logging.Formatter(
+                self.config['logs'].get('format') or '%(asctime)s | %(name)30s | %(levelname)10s | %(message)s'
+            )
+            
+            if self.config['logs'].get('rotating'):
+                file_handler = logging.RotatingFileHandler(
+                    abspath(join(self.config['log_dir'], self.name + '.log')),
+                    'a+',
+                    maxBytes=self.config['logs'].get('max_bytes') or 0,
+                    backupCount=self.config['logs'].get('backup_count') or 0
+                )
+            else:
+                file_handler = logging.FileHandler(abspath(join(self.config['log_dir'], self.name + '.log')), 'a+')
+
+            file_handler.setFormatter(formatter)
+
+            self.logger.addHandler(file_handler)
+    
+    return Sloth
---
+++ logs/setup.py
@@ -0,0 +1,28 @@
+﻿from setuptools import setup
+
+import logs as ext
+
+
+package = 'sloth_ci.ext'
+
+setup(
+    name=ext.__title__,
+    version=ext.__version__,
+    author=ext.__author__,
+    description=ext.__description__,
+    long_description=ext.__long_desciption__,
+    author_email=ext.__author_email,
+    url='https://bitbucket.org/moigagoo/sloth-ci-extensions',
+    py_modules=['%s.%s' % (package, ext.__name__)],
+    package_dir={package: '.'},
+    install_requires = [
+        'sloth_ci>=0.6.2'
+    ],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3']
+    )
