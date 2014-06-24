e9b8bc1f48b091e6050b8e9ec1075089fbec6ea7
Konstantin Molchanov <moigagoo@live.com>
2014-06-24 22:59:39 +0400

    Logs: 1.0.2: Long description updated.

--- logs/logs.py
+++ logs/logs.py
@@ -1,37 +1,37 @@
+'''Sloth CI extension that adds a logger (can be rotating and timed rotating) to Sloth CI apps.
+
+Config params::
+
+    [logs]
+    ;Directory to store the logs. If missing, current directory is used.
+    log_dir = logs
+
+    ;Logging level. Can be either a numeric value (e.g. 10) or a level name (e.g. DEBUG). If missing, Sloth CI's defalt is used (20, INFO). Refer to https://docs.python.org/3.4/library/logging.html#levels.
+    level = 10
+
+    ;Log message format. If missing, the default format is used: %(asctime)s | %(name)30s | %(levelname)10s | %(message)s
+    format = %(asctime)s - %(name)s - %(levelname)s - %(message)s
+
+    ;Rotating or not. If missing, False is considered.
+    rotating = True
+
+    ;Rotating log param. Max filesize before the log is rotated. If missing, 0 is considered, i.e. never rotated.
+    max_bytes = 500
+
+    ;Rotating log param. Max number of log files. If missing, 0 is considered, i.e. unlimited.
+    backup_count = 10
+
+All config params are optional.
+'''
+
+
 __title__ = 'sloth-ci.ext.logs'
 __description__ = 'Logs for Sloth CI apps'
 __long_desciption__ = 'Sloth CI extension that adds a logger (can be rotating) to Sloth CI apps.'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 __author__ = 'Konstantin Molchanov'
 __author_email__ = 'moigagoo@live.com'
 __license__ = 'MIT'
-
-
-"""Sloth CI extension that adds a logger (can be rotating and timed rotating) to Sloth CI apps.
-
-Config params:
-
-[logs]
-;Directory to store the logs. If missing, current directory is used.
-log_dir = logs
-
-;Logging level. Can be either a numeric value (e.g. 10) or a level name (e.g. DEBUG). If missing, Sloth CI's defalt is used (20, INFO). Refer to https://docs.python.org/3.4/library/logging.html#levels.
-level = 10
-
-;Log message format. If missing, the default format is used: %(asctime)s | %(name)30s | %(levelname)10s | %(message)s
-format = %(asctime)s - %(name)s - %(levelname)s - %(message)s
-
-;Rotating or not. If missing, False is considered.
-rotating = True
-
-;Rotating log param. Max filesize before the log is rotated. If missing, 0 is considered, i.e. never rotated.
-max_bytes = 500
-
-;Rotating log param. Max number of log files. If missing, 0 is considered, i.e. unlimited.
-backup_count = 10
-
-All config params are optional.
-"""
 
 
 from os.path import abspath, join
