8f38df037fe22c72d276623fa7ad570267da65f7
Konstantin Molchanov <moigagoo@live.com>
2014-06-24 22:41:44 +0400

    HTML Logs: 1.0.2: Rotating support added; log_dir param added; CSS classes renamed.

--- html_logs/html_logs.py
+++ html_logs/html_logs.py
@@ -1,18 +1,54 @@
 __title__ = 'sloth-ci.ext.html-logs'
 __description__ = 'HTML logs for Sloth CI apps'
-__long_desciption__ = 'Sloth CI extension that adds a HTML logger to Sloth CI apps.'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 __author__ = 'Konstantin Molchanov'
 __author_email__ = 'moigagoo@live.com'
 __license__ = 'MIT'
 
 
-"""Sloth CI extension that adds an extra HTML logger to Sloth CI apps."""
+"""Sloth CI extension that adds an extra HTML logger to Sloth CI apps.
+
+.. important::
+
+    The extension outputs **only** the table rows and columns, **not a complete HTML document**. This output can then be embedded into an HTML document between ``<table>`` and ``<table/>``.
+
+Each log field has it's own CSS class:
+
+log-timestamp
+    Entry timestamp.
+
+log-name
+    Name of the logger posting the entry.
+
+log-level
+    Entry level, e.g. ``INFO``.
+
+log-message
+    Entry message.
+
+Config params:
+
+[html_logs]
+;Directory to store the HTML logs. If missing, current directory is used.
+log_dir = logs/html
+
+;Rotating or not. If missing, False is considered.
+rotating = True
+
+;Rotating log param. Max filesize before the log is rotated. If missing, 0 is considered, i.e. never rotated.
+max_bytes = 500
+
+;Rotating log param. Max number of log files. If missing, 0 is considered, i.e. unlimited.
+backup_count = 10
+
+All config params are optional.
+"""
 
 
 from os.path import abspath, join
 
 import logging
+import logging.handlers
 
 
 def extend(cls):
@@ -20,16 +56,28 @@ def extend(cls):
         def __init__(self, config):
             super().__init__(config)
 
-            html_file_handler = logging.FileHandler(abspath(join(self.config['log_dir'], self.name + '.html')), 'a+')
+            html_log_config = self.config.get('html_logs') or {}
+
+            html_log_dir = html_log_config.get('log_dir') or '.'
 
             html_formatter = logging.Formatter(
 '''<tr>
-    <td class="timestamp">%(asctime)s</td>
-    <td class="name">%(name)s</td>
-    <td class="level">%(levelname)s</td>
-    <td class="message">%(message)s</td>
+    <td class="log-timestamp">%(asctime)s</td>
+    <td class="log-name">%(name)s</td>
+    <td class="log-level">%(levelname)s</td>
+    <td class="log-message">%(message)s</td>
 </tr>'''
             )
+
+            if log_config.get('rotating'):
+                html_file_handler = logging.handlers.RotatingFileHandler(
+                    abspath(join(html_log_dir, self.name + '.html')),
+                    'a+',
+                    maxBytes=html_log_config.get('max_bytes') or 0,
+                    backupCount=html_log_config.get('backup_count') or 0
+                )
+            else:
+                html_file_handler = logging.FileHandler(abspath(join(html_log_dir, self.name + '.html')), 'a+')
 
             html_file_handler.setFormatter(html_formatter)
 
