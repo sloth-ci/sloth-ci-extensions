f55939f824e5b12bdcdbeaeb20bd4ac1141dd913
Konstantin Molchanov <moigagoo@live.com>
2014-11-17 12:21:06 +0300

    SSH exec: Docstrings and sample config updated.

--- ssh_exec/ssh_exec.py
+++ ssh_exec/ssh_exec.py
@@ -6,16 +6,16 @@ Config params::
     module: ssh_exec
 
     # Hosts, comma-delimited. Optional port number can be provided after ':' (if not specified, 22 is used).
-    hosts = ssh.example.com, myserver.com:23
+    hosts: [ssh.example.com, myserver.com:23]
 
     # Username to use for authentication.
-    username = admin
+    username: admin
 
     # Password to use for authentication or to unlock a private key.
-    # password = foobar
+    # password: foobar
 
     # Additional private key files. If not specified, only the keys from the default location are loaded (i.e. ~/.ssh).
-    # keys = ~/my_ssh_keys/key_rsa, somekey
+    # keys: [~/my_ssh_keys/key_rsa, somekey]
 
 Username, password, and keys params are optional.
 '''
