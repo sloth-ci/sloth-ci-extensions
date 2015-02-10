267b420dbccb2da8c742aeb730d209d5d021a1bc
Konstantin Molchanov <moigagoo@live.com>
2015-02-11 01:09:44 +0300

    Build Emal Notifications: 1.0.3: App listen point is used in subject instead of name; updated for Sloth CI 1.2.3.

--- build_email_notifications/setup.py
+++ build_email_notifications/setup.py
@@ -16,7 +16,7 @@ setup(
     py_modules=['%s.%s' % (package, ext.__name__)],
     package_dir={package: '.'},
     install_requires = [
-        'sloth_ci>=1.2.1'
+        'sloth_ci>=1.2.3'
     ],
     license='MIT',
     classifiers=[
