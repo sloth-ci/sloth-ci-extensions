01f9caa8dc93448894a879bfac617cfafdf2e82b
Konstantin Molchanov <moigagoo@live.com>
2015-10-28 22:18:59 +0300

    Docker Exec: 1.1.3: Docs updated to the new format.

--- docker_exec/setup.py
+++ docker_exec/setup.py
@@ -1,4 +1,4 @@
-﻿from setuptools import setup
+from setuptools import setup
 
 import docker_exec as ext
 
@@ -17,7 +17,7 @@ setup(
     packages=[package],
     package_dir={package: '.'},
     install_requires = [
-        'sloth-ci>=1.2.3',
+        'sloth-ci>=2.0.0',
         'docker-py',
         'awesome-slugify'
     ],
