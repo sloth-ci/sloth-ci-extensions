626685498584ac790fcd228470f9874af07a3547
Konstantin Molchanov <moigagoo@live.com>
2015-12-11 18:03:18 +0300

    Devtools: 0.0.1: Initial commit.

---
+++ devtools/devtools.py
@@ -0,0 +1,73 @@
+﻿'''Utilities to help you develop extensions and validators for Sloth CI.
+
+
+Installation
+------------
+
+.. code-block:: bash
+
+    $ pip install sloth-ci.ext.devtools
+
+
+Usage
+-----
+
+.. code-block:: yaml
+    :caption: devtools.yml
+
+    extensions:
+        dev:
+            # Use the module sloth_ci.ext.devtools.
+            module: devtools
+'''
+
+
+__title__ = 'sloth-ci.ext.devtools'
+__description__ = 'Utilities for Sloth CI extension and validator development'
+__version__ = '0.0.1'
+__author__ = 'Konstantin Molchanov'
+__author_email__ = 'moigagoo@live.com'
+__license__ = 'MIT'
+
+
+def extend_bed(cls, extension):
+    return cls
+
+
+def extend_cli(cls, extension):
+    '''Add CLI commands to create extension and validator dev_templates.'''
+
+    from os import mkdir
+    from os.path import join, dirname
+    from shutil import copy
+
+    from cliar import set_name
+
+
+    class CLI(cls):
+        @set_name('create-extension')
+        def create_extension(self, name):
+            '''create an extension template'''
+
+            extension_template = join(dirname(__file__), 'dev_templates/extension/extension.py')
+            setup_py_template = join(dirname(__file__), 'dev_templates/extension/setup.py')
+
+            mkdir(name)
+
+            copy(extension_template, join(name, '%s.py' % name))
+            copy(setup_py_template, name)
+
+        @set_name('create-validator')
+        def create_validator(self, name):
+            '''create a validator template'''
+
+            validator_template = join(dirname(__file__), 'dev_templates/validator/extension.py')
+            setup_py_template = join(dirname(__file__), 'dev_templates/validator/setup.py')
+
+            mkdir(name)
+
+            copy(validator_template, join(name, '%s.py' % name))
+            copy(setup_py_template, name)
+
+
+    return CLI
---
+++ devtools/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+import devtools as ext
+
+
+package = 'sloth_ci.ext'
+
+setup(
+    name=ext.__title__,
+    version=ext.__version__,
+    author=ext.__author__,
+    description=ext.__description__,
+    long_description=ext.__doc__,
+    author_email=ext.__author_email__,
+    url='https://bitbucket.org/moigagoo/sloth-ci-extensions',
+    py_modules=['%s.%s' % (package, ext.__name__)],
+    packages=[package],
+    package_dir={package: '.'},
+    package_data={package: ['dev_templates/*/*.py']},
+    install_requires = [
+        'sloth-ci>=2.0.0'
+    ],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3']
+    )
