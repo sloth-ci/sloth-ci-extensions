a26ea70f73b491f867c9208b4fa869bd792efc79
Konstantin Molchanov <moigagoo@live.com>
2016-01-21 13:25:10 +0300

    Devtools: 1.0.2: The '-destination' param added to specify the directory to create a extension or validator in.

--- devtools/devtools.py
+++ devtools/devtools.py
@@ -36,7 +36,7 @@ Call the ``sci dev`` with ``-e`` or ``-v`` to create an extensions or a validato
 
 __title__ = 'sloth-ci.ext.devtools'
 __description__ = 'Utilities for Sloth CI extension and validator development'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 __author__ = 'Konstantin Molchanov'
 __author_email__ = 'moigagoo@live.com'
 __license__ = 'MIT'
@@ -50,7 +50,7 @@ def extend_cli(cls, extension):
     '''Add CLI commands to create extension and validator templates.'''
 
     from os import mkdir
-    from os.path import join, dirname
+    from os.path import join, dirname, abspath
 
     from string import Template
 
@@ -61,35 +61,37 @@ def extend_cli(cls, extension):
 
             self.template_path = join(dirname(__file__), 'dev_templates')
 
-        def dev(self, name, extension=False, validator=False):
-            '''create an extension or validator template'''
+        def dev(self, name, extension=False, validator=False, destination='.'):
+            '''create an extension or validator template in a given location (current directory by default)'''
 
             if bool(extension) == bool(validator):
                 print('Run "sci dev -e NAME" to create an extension or "sci dev -v NAME" to create a validator.')
 
             else:
-                mkdir(name)
+                destination_path = abspath(join(destination, name))
+
+                mkdir(destination_path)
 
                 if extension:
                     source_template = Template(open(join(self.template_path, 'extension', 'extension.py')).read())
                     setup_py_template = Template(open(join(self.template_path,'extension', 'setup.py')).read())
 
-                    with open(join(name, '%s.py' % name), 'w') as source:
+                    with open(join(destination_path, '%s.py' % name), 'w') as source:
                         source.write(source_template.safe_substitute(extension=name))
 
-                    with open(join(name, 'setup.py'), 'w') as setup_py:
+                    with open(join(destination_path, 'setup.py'), 'w') as setup_py:
                         setup_py.write(setup_py_template.safe_substitute(extension=name))
 
-                    print('Extension "%s" created.' % name)
+                    print('Extension "%s" created in %s' % (name, destination_path))
 
                 elif validator:
                     source_template = Template(open(join(self.template_path, 'validator', 'validator.py')).read())
                     setup_py_template = Template(open(join(self.template_path, 'validator', 'setup.py')).read())
 
-                    with open(join(name, '%s.py' % name), 'w') as source:
+                    with open(join(destination_path, '%s.py' % name), 'w') as source:
                         source.write(source_template.safe_substitute(validator=name))
 
-                    with open(join(name, 'setup.py'), 'w') as setup_py:
+                    with open(join(destination_path, 'setup.py'), 'w') as setup_py:
                         setup_py.write(setup_py_template.safe_substitute(validator=name))
 
                     print('Validator "%s" created.' % name)
