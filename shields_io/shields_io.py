09928ddd0b4755b11de720bdd7c40ca12eed5536
Konstantin Molchanov <moigagoo@live.com>
2016-03-17 12:27:27 +0300

    Shields.io: 1.0.3: Docs improved.

--- shields_io/shields_io.py
+++ shields_io/shields_io.py
@@ -42,8 +42,9 @@ Usage
 
 #.  Use the URL http://host:port/app?action=shield to get your badge.
 
-    You can customize the badge on the fly by passing ``style`` and ``format`` query params:
+    You can customize the badge on the fly by passing ``label``, ``style``, and``format`` query params:
 
+    -   http://host:port/app?action=shield&label=Build%20for%20{app}
     -   http://host:port/app?action=shield&style=social
     -   http://host:port/app?action=shield&format=png
 
@@ -51,7 +52,7 @@ Usage
 
 __title__ = 'sloth-ci.ext.shields_io'
 __description__ = 'Status build shields for Sloth CI powered by http://shields.io'
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 __author__ = 'Konstantin Molchanov'
 __author_email__ = 'moigagoo@live.com'
 __license__ = 'MIT'
