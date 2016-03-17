80c79e30524b05a081cad38b818f6b5f04bb664e
Konstantin Molchanov <moigagoo@live.com>
2016-03-17 14:10:01 +0300

    Shields.io: 1.0.5: {status} and {timestamp} placeholders added.

--- shields_io/shields_io.py
+++ shields_io/shields_io.py
@@ -22,8 +22,11 @@ Usage
                     # Use the module sloth_ci.ext.shields_io.
                     module: shields_io
 
-                    # Badge label. You can use the ``{app}`` placeholder for the app name
-                    # label: My Sloth CI Status for {app} # default is ``Sloth CI: {app}``
+                    # Badge label. You can use the ``{app}`` and ``{timestamp}`` placeholders for app name and build timestamp.
+                    # label: "My Sloth CI Status for {app}" # default is ``Sloth CI: {app}``
+
+                    # Badge status. You can use ``{status}``, ``{app}``, and ``{timestamp}`` placeholders build status, app name, and build timestamp
+                    # status: "{status}" # default is ``{status}, {timestamp}``
 
                     # Badge style: ``plastic``, ``flat``, ``flat-square``, or ``social``
                     # style: social # default is ``flat``
@@ -52,7 +55,7 @@ Usage
 
 __title__ = 'sloth-ci.ext.shields_io'
 __description__ = 'Status build shields for Sloth CI powered by http://shields.io'
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 __author__ = 'Konstantin Molchanov'
 __author_email__ = 'moigagoo@live.com'
 __license__ = 'MIT'
@@ -60,6 +63,8 @@ __license__ = 'MIT'
 
 def extend_bed(cls, extension):
     '''Replace the default app listener with the one that returns a shield from http://shields.io.'''
+
+    from time import ctime
 
     import cherrypy
     from requests import get
@@ -72,6 +77,7 @@ def extend_bed(cls, extension):
             self._shields_config = extension['config']
 
             self._shield_label = self._shields_config.get('label', 'Sloth CI: {app}')
+            self._shield_status = self._shields_config.get('status', '{status}, {timestamp}')
             self._shield_colors = {
                 'INFO': 'brightgreen',
                 'WARNING': 'yellow',
@@ -106,6 +112,8 @@ def extend_bed(cls, extension):
 
                     shield_label = kwargs.get('label', self._shield_label)
 
+                    shield_status = kwargs.get('status', self._shield_status)
+
                     shield_colors = self._shield_colors
                     shield_colors.update(self._shields_config.get('colors', {}))
 
@@ -115,16 +123,24 @@ def extend_bed(cls, extension):
 
                     info = self.info({'listen_point': listen_point})
                     status, level = info['last_build_status_message'], info['last_build_status_level']
+                    timestamp = ctime(info['last_build_timestamp'])
 
                     shield_url = (
                         'https://img.shields.io/badge/' +
                         '-{status}-{color}.{format}' +
                         '?label={label}&style={style}'
                     ).format(
-                        status=status,
+                        status=shield_status.format(
+                            app=listen_point,
+                            timestamp=timestamp,
+                            status=status
+                        ),
                         color=shield_colors[level],
                         format=shield_format,
-                        label=shield_label.format(app=listen_point),
+                        label=shield_label.format(
+                            app=listen_point,
+                            timestamp=timestamp
+                        ),
                         style=shield_style
                     )
 
